118e34b88ba83c7387be708f713a593dee97c8b3
BarbozahLuis <124843843+BarbozahLuis@users.noreply.github.com>
2023-05-31 00:53:38 +0000

    30/05

---
+++ POOExercicio2/Agenda.java
@@ -0,0 +1,29 @@
+package POOExercicio2;
+
+public class Agenda {
+    //atributos
+    String nome;
+    int idade;
+    double altura;
+    //construtor default (vazio)
+    //sets and gets
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+}
---
+++ POOExercicio2/App.java
@@ -0,0 +1,37 @@
+package POOExercicio2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) {
+        Random rd = new Random(0);
+        Scanner sc = new Scanner(System.in);
+        // vetor de objetos
+        Agenda pessoas[] = new Agenda[10]; // pessoas = vetor/array e foi determinado o seu tamanho
+        // preencher o meu vetor/array
+        for (int i = 0; i < pessoas.length; i++) {// fori para preencher as 10 posições dos vetores {
+            // construtor de objetos
+            pessoas[i] = new Agenda();
+            // preencher os atributos
+            pessoas[i].setNome("Pessoa" + i);
+            pessoas[i].setAltura(i);
+            pessoas[i].setIdade(i + 18);
+        }
+        // buscador
+        // enquanto nomeDigitado != nome do Objeto
+        // cont e procure
+        int cont = 0;
+        boolean teste=true;
+        String nomeDigitado = sc.next();// scanner ou JOptionPane
+        while (!nomeDigitado.equalsIgnoreCase(pessoas[cont].getNome())) //equals é pra comparar string
+        {
+
+            cont++;
+        }
+        System.out.println("Nome: " + pessoas[cont].getNome() + "Idade: " + pessoas[cont].getIdade() + "Altura: "
+                + pessoas[cont].getAltura());
+    }
+}
