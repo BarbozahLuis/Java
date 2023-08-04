791beb208fc16f60d58f3d03aaf05aa0357e7dad
luishbarboza0610@gmail.com <luishbarboza0610@gmail.com>
2023-08-03 22:01:02 -0300

    03/08

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -6,16 +6,30 @@ public class ExemploThrow {
 
     public static void main(String[] args) {
             String senhaSecreta = "123456";
+            String login = JOptionPane.showInputDialog("Informe login");
+            String dataNascimento = JOptionPane.showInputDialog("Informe data nascimento [dd/mm/aa]");
             String senhaDigitada = JOptionPane.showInputDialog("Informe senha de 6 digitos");
+            
+            dataNascimento = dataNascimento.replace("/", "");
+            
         try{
-            if(!senhaDigitada.equals(senhaSecreta)){
+            if(senhaDigitada.length()!=6) {
                 JOptionPane.showMessageDialog(null, "Senha incorreta!");
-                throw new Exception("Senha invalina");
-            }else{
+                throw new Exception("Senha não tem 6 digitos");
+            }if(login.equals(senhaDigitada)){
+                JOptionPane.showMessageDialog(null, "Senha incorreta!");
+                throw new Exception("Senha não pode ser identica ao login");
+            }if(senhaDigitada.equals(dataNascimento)){
+                JOptionPane.showMessageDialog(null, "Senha incorreta!");
+                throw new Exception("Senha não pode ser identica a data de nascimento");
+            }
+            else{
                 JOptionPane.showMessageDialog(null, "Senha correta!" + "\nBem vindo!");
             }
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Digite uma senha com 6 digitos");
+        }
+        
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
         }
 }
