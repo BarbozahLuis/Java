e74cb43fe8befdde1698b72468c5b7cde5500b86
BarbozahLuis <luishbarboza0610@gmail.com>
2023-08-14 22:00:47 -0300

    14/08

---
+++ GUIInicio/src/FrameAWT.java
@@ -0,0 +1,10 @@
+import java.awt.Frame;
+
+public class FrameAWT extends Frame{
+    public FrameAWT(){
+        super();
+        this.setSize(300,300);
+        this.setVisible(true);
+        
+    }
+}
